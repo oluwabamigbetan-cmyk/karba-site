c002fda250a35985c899a63df5b6748b0df51c2e
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-10 15:38:16 +0100

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-window.KARBA_CONFIG = { BACKEND_URL: 'https://karba-backend-jgbp.onrender.com' };
+// config.js
+window.KARBA_CONFIG = {
+  BACKEND_URL: 'https://karba-backend-jgbp.onrender.com',
+  RECAPTCHA_SITE_KEY: 6Lcb58QrAAAAAARTzK6eZGGJXXd2WgaZaUT4dg2C
+};
